58ec8afb4b259e1a8f24984290935027c8db848d
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-09-28 22:00:09 -0700

    1

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 # ******************************
 # Make your Code
 # ******************************
+import random 
+
+numbers = []
+for i in range(10):
+	numbers.append(random.randint(0,20))
+print ("Before sorted\n", numbers)
+
+insert_val = int(input("Enter one integer"))
+#numbers = sorted(numbers)
+numbers.sort()
+
+print ("Before insertion\n", numbers)
+
+
+for i in range(len(numbers)):
+	if ( insert_val < numbers[i]):
+		numbers.insert(i, insert_val)
+		break
+else:
+	numbers.append(insert_val)
+
+print ("After insertion\n", numbers)
+
+
